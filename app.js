271fc47beae7ac50c521a975288a38046d892ccc
aymanezahir2 <aymanezahir2@gmail.com>
2022-03-24 02:24:17 +0100

    change

--- app.js
+++ app.js
@@ -5,52 +5,57 @@ const app = express();
 const port = 5000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+const schemas = require("./models/aricleSchema");
 // for auto refresh
-{const path = require("path");
-const livereload = require("livereload");
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, "public"));
+{
+  const path = require("path");
+  const livereload = require("livereload");
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "public"));
 
-const connectLivereload = require("connect-livereload");
-app.use(connectLivereload());
+  const connectLivereload = require("connect-livereload");
+  app.use(connectLivereload());
 
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
 }
 // mongoose
-{const mongoose = require("mongoose");
+{
+  const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb+srv://aymanezahir2:zahirhub2@cluster0.qpl87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
-  .then((result) => {
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
+  mongoose
+    .connect(
+      "mongodb+srv://aymanezahir2:zahirhub2@cluster0.qpl87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    .then((result) => {
+      app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+      });
+    })
+
+    .catch((err) => {
+      console.log(err);
     });
-  })
-
-  .catch((err) => {
-    console.log(err);
-  });
-
-
 }
 
+app.post("/all-articles", function (req, res) {
+  const articles = new schemas(req.body);
 
+  articles
+  .save()
+  .then(function(result){
+    res.redirect("/all-articles")
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+});
 
-
-
-
-
-
-
-
-  
 app.get("/", (req, res) => {
   res.redirect("/all-articles");
 });
@@ -62,9 +67,6 @@ app.get("/all-articles", (req, res) => {
 app.get("/add-new-article", (req, res) => {
   res.render("add-new-article");
 });
-
-
-
 
 //  404
 app.use((req, res) => {
